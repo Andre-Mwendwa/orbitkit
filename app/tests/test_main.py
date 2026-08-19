78d5bb48af11b3f2c33ca1c3e58ce1a20a7193e9
Dre-AsiliVentures <ghdmwendwa01@gmail.com>
2026-08-19 07:30:29 +0300

    Assert mod

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -6,5 +6,5 @@ def test_health_endpoint():
     client = app.test_client()
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.get_json()["status"] == "ok"
+    assert response.get_json()["status"] == "Sawa"
     
